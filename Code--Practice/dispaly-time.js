a8d59c4a778b2b52fcbbef84d38e8c1ae8450b4b
quixlab <quixlab.com.com>
2019-06-29 22:12:39 +0600

    dispaly-time

---
+++ Code--Practice/dispaly-time.js
@@ -0,0 +1,42 @@
+// Dispaly the time
+
+var today = new Date();
+var hour = today.getHours();
+var day = today.getDay();
+var minutes = today.getMinutes();
+var second = today.getSeconds();
+
+
+var prepand = (hour >= 12)? "PM" : "AM";
+
+var format = (hour => 12)? hour - 12 : hour;
+
+
+
+if(hour === 0 && prepand === "AM"){
+    if(minutes === 0 && second === 0){
+        hour = 12;
+        prepand = "Midnight"
+    }else{
+        hour = 12;
+        prepand = "AM"
+    }
+}
+
+
+if(hour === 0 && prepand === "PM"){
+    if(minutes === 0 && second === 0){
+        hour = 12;
+        prepand = "Noon";
+    }else{
+        hour = 12;
+        prepand = "PM";
+    }
+}
+
+
+
+
+
+console.log(`Current time : ${format} ${prepand} : ${minutes} ${second}`);
+console.log(`Current Hour : ${hour}`);
