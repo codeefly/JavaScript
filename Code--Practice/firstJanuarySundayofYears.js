43a23a6408a136f7ef47984937d720d5289a82ce
quixlab <quixlab.com@gmail.com>
2019-09-17 13:23:52 +0600

    practiced

---
+++ Code--Practice/firstJanuarySundayofYears.js
@@ -0,0 +1,16 @@
+// for (var year = 2014; year <= 2050; year ++){
+//     var d = new Date(year, 0, 1);
+//     if(d.getDay() === 0){
+//         console.log(`1st Day January is being a Sunday ${year}`);
+//     }
+// }
+
+
+
+
+for(var year = 2020; year <= 2070; year ++){
+    var d = new Date(year, 0, 1);
+    if(d.getDay() === 0){
+        console.log(`${year}`);
+    }
+}
