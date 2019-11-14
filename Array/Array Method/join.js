0a68072b0ddbebfae7447449bdb2554f6755606c
quixlab <quixlab.com@gmail.com>
2019-11-14 12:11:35 +0600

    join practice

---
+++ Array/Array Method/join.js
@@ -0,0 +1,14 @@
+// The join() method creates and returns a new string by concatenating all of the elements in an array (or an array-like object), separated by commas or a specified separator string
+
+// separated by commas or a specified separator string
+
+
+
+const elements = ['Fire', 'Air', 'Water'];
+
+console.log(elements.join());
+console.log(elements.join(''));
+console.log(elements.join(' '));
+console.log(elements.join('-'));
+console.log(elements.join(' + '));
+console.log(elements.join(', '));
