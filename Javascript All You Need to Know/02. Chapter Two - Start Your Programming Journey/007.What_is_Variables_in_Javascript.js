3cd1728f88e3703d1db199d37c1c16a15dcd92d0
quixlab <quixlab.com@gmail.com>
2020-06-24 13:42:22 +0600

    file rename

---
+++ Javascript All You Need to Know/02. Chapter Two - Start Your Programming Journey/007.What_is_Variables_in_Javascript.js
@@ -0,0 +1,6 @@
+var name = 'Saiful Islam';
+var age = 28;
+
+console.log(name + ' Learning JavaScript');
+console.log('My age is ' + age);
+
---
+++ Javascript All You Need to Know/02. Chapter Two - Start Your Programming Journey/009._What_is_Data_Types_in_Javascript.js
@@ -0,0 +1,20 @@
+// 2 types of Data
+// 1. Primitive
+// 2. Object 
+
+// Primitive is Javascript defined data type
+// Object is user defined data type
+
+
+// Primitive
+//  => Number
+//  => String
+//  =? Boolean
+//  => Undefined
+//  => Null
+
+
+// Object Type
+//  => Array
+//  => Object
+//  => Function
